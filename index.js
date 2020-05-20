3df3fcc5fa1f4bfe486fab73af33e2a8cec539b8
Abhinav Monga <abhimonga16@gmail.com>
2020-05-20 15:06:21 +0530

    Final Upload

---
+++ index.js
@@ -0,0 +1,159 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json()); // creates express http server
+const request = require('request');
+const https = require('https');
+const weather=require('./weather');
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {
+
+    let body = req.body;
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+               if(webhook_event.message){
+                   handle_message(webhook_event.sender.id,webhook_event.message);
+               }
+            //    else {
+            //        handlePost(webhook_event.sender.id,webhook_event.postback);
+            //    }
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+
+});
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "ABHINAV16MONGA"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.send("OK");
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+            res.send("Error");
+        }
+    }
+});
+
+function handle_message(sender_id,mes){
+    var res;
+    if(mes.text!='Hi' && mes.text!='hi' && mes.text!='HI' &&  mes.text!='Hello' && mes.text!='Hey'&& !(mes.attachments)){
+
+        var x=weather(mes.text);
+        x.then(function(result){
+           var  usr=result;
+           var  temp=usr.main.temp;
+           var sky=usr.weather[0].description;
+           var humid=usr.main.humidity;
+           var wind=usr.wind.speed;
+            // var temperature=toString(temp);
+           temp=temp-273.15;
+           temp=temp.toFixed(2);
+            res={
+                "text":`Weather Details in ${mes.text} is as follow:\n Sky is ${sky}\n Temperature is  ${temp} deg Celcius\n Humidity is ${humid}%  and Wind Speed is ${wind}Kmph`
+            }
+            sendMsg(sender_id,res);
+        
+        },function(err){
+            res={
+                "text":`I am afraid that I can't help you with this `
+            }
+            sendMsg(sender_id,res);
+        }).catch(()=>{
+            res={
+                "text":`I am afraid that I can't help you with this `
+            }
+            sendMsg(sender_id,res);
+        })
+       
+        
+       
+    }
+     if(mes.text==='Hi' || mes.text==='hi' || mes.text==='HI'|| mes.text==='Hello' || mes.text==='Hey'){
+        res={
+            "text":`Hi ,Please Enter desired City name to get weather details`
+        }
+        sendMsg(sender_id,res);
+    }
+    if (mes.attachments) {
+  
+        // Gets the URL of the message attachment
+        var loc = mes.attachments[0].payload.title;
+        console.log(loc);
+
+        var x=weather(loc);
+        x.then(function(result){
+           var  usr=result;
+           var  temp=usr.main.temp;
+            // var temperature=toString(temp);
+           temp=temp-273.15;
+           temp=temp.toFixed(2);
+           var sky=usr.weather[0].description;
+           var humid=usr.main.humidity;
+           var wind=usr.wind.speed;
+            res={
+                "text":`Weather Details in ${loc} is as follow:\n Sky is ${sky}\n Temperature is  ${temp} deg Celcius\n Humidity is ${humid}%  and Wind Speed is ${wind}Kmph`
+            }
+            sendMsg(sender_id,res);
+        
+        },function(err){
+            res={
+                "text":`I am afraid that I can't help you with this ${loc}`
+            }
+            sendMsg(sender_id,res);
+        }).catch(()=>{
+            res={
+                "text":`I am afraid that I can't help you with this ${loc}`
+            }
+            sendMsg(sender_id,res);
+        })
+     
+      } 
+  //  sendMsg(sender_id,res);
+}
+function sendMsg(sender_id,res){
+    let request_body = {
+        "recipient": {
+          "id": sender_id
+        },
+        "message": res
+      }
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": "EAAD288GBsZAIBAF937Qohw4Ala8CSYejaydwWH2ZCySR4ZCpJAtvUHTzXBzHI2XQyjQPzV1LTZA2cHobSmUd5fmIJkpDg1Cc8mpwMz7tJ09hXmcU2ZAt2j45MrStAl6fnEWVvZBlzaNl8Rda5zRbk57Bh2GpDGlv62Lv0dqAvPxZAxpgAXmvOYs" },
+        "method": "POST",
+        "json": request_body
+      }, (err, resP, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
+}
---
+++ weather.js
@@ -0,0 +1,40 @@
+const https = require('https');
+var usr;
+function get_result(query){
+	return new Promise(function(resolve,reject){
+
+		https.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=d3de9c566542cc933b9215952ec573ec`, (resp) => {
+			let data = '';
+		  
+			// A chunk of data has been recieved.
+			resp.on('data', (chunk) => {
+			  data += chunk;
+			});
+		  
+			// The whole response has been received. Print out the result.
+			resp.on('end', () => {
+			  // console.log(typeof(data));
+			  resolve(JSON.parse(data));
+			  //  return data;
+			});
+		  
+		  }).on("error", (err) => {
+			reject(err);
+		  });
+	
+	})
+	
+}
+	// var x=get_result("Mumbai");
+	// x.then(function(result){
+	// 	 usr=result;
+	// 	console.log(usr["main"].temp);
+	// 	var temperature=toString(usr["main"].temp)
+	// 	console.log(temperature)
+	// },function(err){
+	// 	console.log(err);
+	// })
+	
+
+
+module.exports=get_result
